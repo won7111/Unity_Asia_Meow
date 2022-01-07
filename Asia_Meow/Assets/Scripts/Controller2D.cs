54715accd557491866455262580305d2dadc84ff
won7111 <hideyoshi881117@gmail.com>
2022-01-08 03:08:24 +0800

    設定地板layer

--- Asia_Meow/Assets/Scripts/Controller2D.cs
+++ Asia_Meow/Assets/Scripts/Controller2D.cs
@@ -11,12 +11,37 @@ public class Controller2D : MonoBehaviour
     public float speed = 3.5f;
     [Header("跳躍高度"), Range(0, 15000)]
     public float jump = 300;
+    [Header("檢查地板尺寸與位移")]
+    [Range(0, 1)]
+    public float checkGroundRadius = 0.1f;
+    public Vector3 checkGroundOffset;
+    [Header("跳躍按鍵與可跳躍圖層")]
+    public KeyCode keyJump = KeyCode.Space;
+    public LayerMask canJumpLayer;
 
     #endregion
     /// <summary>
     /// 剛體元件 Rigidbody2D
     /// </summary>
     private Rigidbody2D rig;
+    /// <summary>
+    /// 繪製圖示
+    /// 在Unity 繪製輔助用的圖示
+    /// 線條、射線、圓形、方形
+    /// 圖示 Gizmos 類別
+    /// </summary>
+    private void OnDrawGizmos()
+        
+    {
+        //1.決定圖示顏色
+        Gizmos.color = new Color(1, 0, 0.2f, 0.3f);
+        //2.決定繪製圖形
+        // transform.position此物件的世界座標
+        // transform.TransformDirection()根據變形元件的區域座標轉為世界座標
+        Gizmos.DrawSphere(transform.position +
+            transform.TransformDirection (checkGroundOffset),  checkGroundRadius);
+
+    }
     private void Start()
     {
         //剛體欄位 = 取得元件<2D 剛體>()
@@ -33,6 +58,10 @@ public class Controller2D : MonoBehaviour
         Move();
     }
 
+    private void Update()
+    {
+        Flip();
+    }
     #region 方法
     /// <summary>
     /// 1.玩家是否有按移動按鍵 左右方向鍵 或 A、D
@@ -43,6 +72,28 @@ public class Controller2D : MonoBehaviour
         // h值 指定為 輸入. 取得軸向(水平軸) - 水平軸代表左右鍵與 AD
         float h = Input.GetAxis("Horizontal");
         print("玩家左右按鍵值" + h);
+
+        //剛體元件.加速度 = 新 二維向量(h 值 * 移動速度，剛體，加速度，垂直 );
+        rig.velocity = new Vector2(h*speed, rig.velocity.y);
+    }
+    
+    /// <summary>
+    /// 翻面 :
+    /// h 值 小於 0 :左 : 角度 Y 180 
+    /// h 值 大於 0 :右 : 角度 Y 0
+    /// </summary>
+    private void Flip() 
+    {
+        float h = Input.GetAxis("Horizontal");
+        if (h < 0)
+        // h 值 小於 0 :左 : 角度 Y 180
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        else if ( h > 0)
+        {
+            transform.eulerAngles = Vector3.zero;
+        }
     }
     #endregion
 
