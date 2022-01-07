e5789f48bfeafaa8a4871b6b8c00fff2aede694a
won7111 <hideyoshi881117@gmail.com>
2022-01-07 22:27:33 +0800

    角色控制器與水平軸

--- Asia_Meow/Assets/Scripts/Controller2D.cs
+++ Asia_Meow/Assets/Scripts/Controller2D.cs
@@ -13,5 +13,40 @@ public class Controller2D : MonoBehaviour
     public float jump = 300;
 
     #endregion
+    /// <summary>
+    /// 剛體元件 Rigidbody2D
+    /// </summary>
+    private Rigidbody2D rig;
+    private void Start()
+    {
+        //剛體欄位 = 取得元件<2D 剛體>()
+        rig = GetComponent<Rigidbody2D>();
+
+    }
+    /// <summary>
+    /// Update 約 60 FPS
+    /// 固定更新事件 : 50 FPS
+    /// 處理物理行為
+    /// </summary>
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    #region 方法
+    /// <summary>
+    /// 1.玩家是否有按移動按鍵 左右方向鍵 或 A、D
+    /// 2.物件移動行為 (API)
+    /// </summary>
+    private void Move()
+    {
+        // h值 指定為 輸入. 取得軸向(水平軸) - 水平軸代表左右鍵與 AD
+        float h = Input.GetAxis("Horizontal");
+        print("玩家左右按鍵值" + h);
+    }
+    #endregion
+
 
 }
+
+
